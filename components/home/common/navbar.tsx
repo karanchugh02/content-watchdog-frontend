b677f551619add28aa607cfcdb73d7327e6f5918
gauri82 <gauri0231.be20@chitkara.edu.in>
2023-05-02 23:19:01 +0530

    ci: updated body v2

--- components/home/common/navbar.tsx
+++ components/home/common/navbar.tsx
@@ -48,7 +48,6 @@ const NavBar = () => {
           }`}
         ></span>
       </button>
-      
 
       <nav>
         <CustomLink
@@ -81,7 +80,6 @@ const NavBar = () => {
           className="font-bold px-2 py-4 rounded-full  border-2 border-white hover:border-black hover:bg-white hover:text-black"
         ></CustomLink>
       </nav>
-      
     </header>
   );
 };
