3cac8eb9469fca9e508c77907c8c8f67f514f8b7
dikshant4901 <dikshant1865.be20@chitkara.edu.in>
2023-05-04 15:07:52 +0530

    ci: project build

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -46,17 +46,17 @@ function Page() {
     <>
       <section className="bg-black  ">
         <div className="flex flex-col items-center justify-center space-x-5 px-6 py-7 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-        >
-          <img
-            className="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-            alt="logo"
-          />
-          ContentWatchDog
-        </a>
+          <a
+            href="#"
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img
+              className="w-8 h-8 mr-2"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+              alt="logo"
+            />
+            ContentWatchDog
+          </a>
 
           <div className="w-full  bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
